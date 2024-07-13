4c7bf9f2a855772de26d313406746ed5a3112519
Vishal Bist <bishtvishal390@gmail.com>
2024-07-13 23:48:05 +0530

    update url

--- data/cart_items.js
+++ data/cart_items.js
@@ -1,14 +1,14 @@
 export const cart_items = [{
   delivery_date: 'Monday June,2',
   ProductId: 1,
-  ProductImage: '../images/products/men-golf-polo-t-shirt-blue.jpg',
+  ProductImage: 'images/products/men-golf-polo-t-shirt-blue.jpg',
   ProductName: "Men's Regular-Fit Quick Dry Golf Polo Shirt",
   ProductPrice: 7990,
   quantity: 1
 },{
   delivery_date: 'Monday June,2',
   ProductId: 2,
-  ProductImage: '../images/products/men-athletic-shoes-green.jpg',
+  ProductImage: 'images/products/men-athletic-shoes-green.jpg',
   ProductName: "Men's Athletic Sneaker",
   ProductPrice: 3890,
   quantity: 1
